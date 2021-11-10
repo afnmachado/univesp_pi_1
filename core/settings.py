f01aa184897aadc539e95ec48666e4b48540fab6
afnmachado <2008584@aluno.univesp.br>
2021-11-10 20:57:55 -0300

    Teste Heroku RDS

--- core/settings.py
+++ core/settings.py
@@ -160,5 +160,10 @@ MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=0, ssl_require=False)
 # Activate Django-Heroku.
-django_heroku.settings(locals())
+django_heroku.settings(locals() ,databases=False)
+DATABASES['default'].update(db_from_env)
